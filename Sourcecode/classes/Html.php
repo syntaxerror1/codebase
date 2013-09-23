75122894220b215e4becac397b44fa2375c818f5
Chris <clutz@syntaxerror.org.uk>
2013-09-23 21:21:20 +0100

    Added Url function to Html class

--- Sourcecode/classes/Html.php
+++ Sourcecode/classes/Html.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This class allows you to create HTML elements using functions.
+ * This will make sure all HTML elements are the same each time.
+ * Such as all local URLs including the domain instead of starting "/"
+ * 
+ * @author Chris Cutts
+ *
+ */
 class Html  {
 	
+	function url($url, $linkText, $attributes=array()) {
+		
+		return $link;
+	}
 }
